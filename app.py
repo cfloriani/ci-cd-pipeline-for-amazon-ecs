82c384f8e58c5a964190ebcc319ab69d39f3c505
Floriani <carlos.floriani@sci10.com.br>
2020-07-23 17:42:04 -0300

    origin/master

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 """Main application file"""
 from flask import Flask
 app = Flask(__name__)
@@ -9,3 +10,16 @@ def returnBackwardsString(random_string):
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
+=======
+#!/usr/bin/env python3
+
+from aws_cdk import core
+
+from ecs_devops_sandbox_cdk.ecs_devops_sandbox_cdk_stack import EcsDevopsSandboxCdkStack
+
+
+app = core.App()
+EcsDevopsSandboxCdkStack(app, "ecs-devops-sandbox-cdk")
+
+app.synth()
+>>>>>>> gerar-error

3714bb5fc8d07dcac13cbe068e7d3a9f0875f8d0
Floriani <carlos.floriani@sci10.com.br>
2020-07-23 17:45:13 -0300

    evitar-erro

--- app.py
+++ app.py
@@ -1,25 +1,19 @@
-<<<<<<< HEAD
-"""Main application file"""
+"""
+Main application file
+"""
 from flask import Flask
+import logging
 app = Flask(__name__)
+
+# Initialize Logger
+LOGGER = logging.getLogger()
+LOGGER.setLevel(logging.INFO)
 
 @app.route('/<random_string>')
 def returnBackwardsString(random_string):
     """Reverse and return the provided URI"""
+    LOGGER.info('Received a message: %s', random_string)
     return "".join(reversed(random_string))
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
-=======
-#!/usr/bin/env python3
-
-from aws_cdk import core
-
-from ecs_devops_sandbox_cdk.ecs_devops_sandbox_cdk_stack import EcsDevopsSandboxCdkStack
-
-
-app = core.App()
-EcsDevopsSandboxCdkStack(app, "ecs-devops-sandbox-cdk")
-
-app.synth()
->>>>>>> gerar-error
